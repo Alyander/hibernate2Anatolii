5aaa9682e464693d2ec47ab43b2b5bc229dfb209
Alyander <momoroot33@gmail.com>
2025-06-22 14:18:31 +0000

    completed Staff.java

--- src/main/java/org/domain/Staff.java
+++ src/main/java/org/domain/Staff.java
@@ -18,5 +18,26 @@ public class Staff {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private short id;
-
+    @Column(name = "first_name", nullable = false, length = 45)
+    private String firstName;
+    @Column(name = "last_name", nullable = false, length = 45)
+    private String lastName;
+    @OneToOne
+    @JoinColumn(name = "address_id")
+    private Address address;
+    @Column(name = "picture")
+    private java.sql.Blob picture;
+    @Column(name = "email", nullable = false, length = 50)
+    private String email;
+    @OneToOne
+    @JoinColumn(name = "store_id")
+    private Store store;
+    @Column(name = "active", length = 1, nullable = false)
+    private short active;
+    @Column(name = "username", nullable = false, length = 16)
+    private String username;
+    @Column(name = "password", nullable = false , length = 40)
+    private String password;
+    @Column(name = "last_update", nullable = false)
+    private java.sql.Timestamp lastUpdate;
 }
