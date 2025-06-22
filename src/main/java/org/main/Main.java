5ce03143edbceff93cac5a0829e43138373f721c
Alyander <momoroot33@gmail.com>
2025-06-22 15:20:11 +0000

    errors catch me

--- src/main/java/org/main/Main.java
+++ src/main/java/org/main/Main.java
@@ -1,11 +1,20 @@
 package org.main;
 
+import org.domain.Film;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
-public class Main {
-    private final SessionFactory sessionFactory = new Configuration().configure().addPackage("domain").buildSessionFactory();
-    public static void main(String[] args) {
+import java.util.List;
+import java.util.Set;
 
+public class Main {
+    private static final SessionFactory sessionFactory = new Configuration().configure().addPackage("domain").buildSessionFactory();
+    public static void main(String[] args) {
+        List<Film> set;
+        try (Session session = sessionFactory.openSession()) {
+            set = session.createQuery("from Film f", Film.class).list();
+        }
+        System.out.println(set);
     }
 }
