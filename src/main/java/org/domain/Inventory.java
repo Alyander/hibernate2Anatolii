e540feccfa58cd33f102f94cd2ec312fdb8602d3
Alyander <momoroot33@gmail.com>
2025-06-22 14:30:34 +0000

    wrote Inventory.java

---
+++ src/main/java/org/domain/Inventory.java
@@ -0,0 +1,26 @@
+package org.domain;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "inventory")
+public class Inventory {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private short inventory_id;
+    @OneToOne
+    @JoinColumn(name="film_id")
+    private Film film;
+    @OneToOne
+    @JoinColumn(name="store_id")
+    private Store store;
+    @Column(name = "last_update")
+    private java.sql.Timestamp lastUpdate;
+}
