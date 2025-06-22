3cb1ffc327cb5b35434a5cd5eee6f1aa02a3695b
Alyander <momoroot33@gmail.com>
2025-06-22 14:54:16 +0000

    wrote payment

---
+++ src/main/java/org/domain/Payment.java
@@ -0,0 +1,37 @@
+package org.domain;
+import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.sql.Timestamp;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "payment")
+public class Payment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private short payment_id;
+    @OneToOne
+    @JoinColumn(name = "customer_id")
+    private Customer customer;
+    @OneToOne
+    @JoinColumn(name = "staff_id")
+    private Staff product;
+    @OneToOne
+    @JoinColumn(name = "rentai_id")
+    private Rentai rentai;
+    @Column(name = "amount")
+    private double amount;
+    @Column(name = "payment_date")
+    private java.sql.Date paymentDate;
+    @Column(name = "last_update")
+    private Timestamp lastUpdate;
+}
