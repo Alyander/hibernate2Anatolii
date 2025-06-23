f69729a3cf8624964a966a4b923b02afcea7850c
Alyander <momoroot33@gmail.com>
2025-06-23 15:33:53 +0000

    Please anybody kill the code

--- src/main/java/org/main/DBControl.java
+++ src/main/java/org/main/DBControl.java
@@ -1,38 +1,29 @@
 package org.main;
 
 import org.domain.Address;
-import org.domain.City;
 import org.domain.Customer;
 import org.domain.Store;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
-import org.hibernate.query.Query;
-
 import java.sql.Date;
 import java.sql.Timestamp;
-import java.util.Random;
 
 public class DBControl {
     private final SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
-    private final Random random = new Random();
     public DBControl() {
     }
 
     public void createNewCustomer(String firstname,String LastName, String email) {
         try (Session session = sessionFactory.openSession()) {
             session.beginTransaction();
-            Address address = session.createQuery(" from Address where address='23 Workhaven Lane'", Address.class)
-                    .getSingleResult();
-            Store store = session.createQuery("from Store where store_id=1", Store.class).getSingleResult();
-//            session.merge(address);
-//            session.merge(store);
+//            Store store =
+            //session.merge(address);
+            //session.merge(store);
             Customer customer = new Customer
-                    ((short)999,store,firstname,LastName,email,address,1,Date.valueOf("2023-05-06"),
+                    ((short)999,session.createQuery("from Store where store_id=2", Store.class).getSingleResult(),firstname,LastName,email,session.createQuery(" from Address where address='28 MySQL Boulevard'", Address.class)
+                            .getSingleResult(),1,Date.valueOf("2023-05-06"),
                             new Timestamp(1750680629));
-//            session.merge(address);
-//            session.merge(store);
-            session.merge(customer);
             session.persist(customer);
             session.getTransaction().commit();
         }
