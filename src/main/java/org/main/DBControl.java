a6daab85233013535b1c9d4b816b31e97c5cb684
Alyander <momoroot33@gmail.com>
2025-06-23 15:25:03 +0000

    AAAAAAA!!!!!

--- src/main/java/org/main/DBControl.java
+++ src/main/java/org/main/DBControl.java
@@ -11,25 +11,30 @@ import org.hibernate.query.Query;
 
 import java.sql.Date;
 import java.sql.Timestamp;
+import java.util.Random;
 
 public class DBControl {
     private final SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
-
+    private final Random random = new Random();
     public DBControl() {
     }
 
-    public Customer createNewCustomer(String firstname,String LastName, String email) {
-        Customer c;
+    public void createNewCustomer(String firstname,String LastName, String email) {
         try (Session session = sessionFactory.openSession()) {
             session.beginTransaction();
             Address address = session.createQuery(" from Address where address='23 Workhaven Lane'", Address.class)
                     .getSingleResult();
-            Store store = session.createNativeQuery("SELECT * from store where store_id=1", Store.class).getSingleResult();
-            Customer customer = new Customer((short) 101, store,firstname,LastName,email,address,1,Date.valueOf("2023-05-06"), Timestamp.valueOf("2023-05-06"));
-            c =customer;
+            Store store = session.createQuery("from Store where store_id=1", Store.class).getSingleResult();
+//            session.merge(address);
+//            session.merge(store);
+            Customer customer = new Customer
+                    ((short)999,store,firstname,LastName,email,address,1,Date.valueOf("2023-05-06"),
+                            new Timestamp(1750680629));
+//            session.merge(address);
+//            session.merge(store);
+            session.merge(customer);
             session.persist(customer);
             session.getTransaction().commit();
         }
-        return c;
     }
 }
